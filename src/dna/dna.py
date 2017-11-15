8ba326b3de3153744705fadd69775b4629644af0
Isaac Anthony <ianthony@optoro.com>
2017-11-15 16:49:54 -0500

    One-hot encode using numpy.

--- src/dna/dna.py
+++ src/dna/dna.py
@@ -1,20 +1,27 @@
 import numpy as np
 import tensorflow.contrib.keras as keras
 
-dataset = np.loadtxt('dna/dna.csv', delimiter=',')
+dataset = np.loadtxt('dna/dna.csv', dtype=int, delimiter=',')
 
-X = keras.backend.one_hot(dataset[:, 1:], 4)
+X = np.eye(4)[dataset[:, 1:]]
 Y = dataset[:, 0]
 
+print(X.shape)
+print(Y.shape)
+
 model = keras.models.Sequential([
-  keras.layers.Conv1D(64, 3, input_shape=X.shape[1:], activation='relu'),
-  keras.layers.Conv1D(32, 3, activation='relu'),
-  keras.layers.MaxPooling1D(pool_size=2),
-  keras.layers.Dropout(0.2),
+  keras.layers.Conv1D(64, 20, input_shape=X.shape[1:]),
+  keras.layers.Activation('relu'),
+  keras.layers.MaxPooling1D(pool_size=4),
+  keras.layers.Conv1D(32, 20),
+  keras.layers.Activation('relu'),
+  keras.layers.MaxPooling1D(pool_size=4),
   keras.layers.Flatten(),
   keras.layers.Dense(12, activation='relu'),
   keras.layers.Dense(1, activation='sigmoid')
 ])
+
+print(model.summary())
 
 model.compile(loss='binary_crossentropy',
               optimizer='adam',
