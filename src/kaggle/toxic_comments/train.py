519d38bbb103d37caf5dfc536f5686a263be50f1
Isaac Anthony <ianthony@optoro.com>
2018-01-05 16:00:14 -0500

    Split kaggle toxic comments into train/test sets.

--- src/kaggle/toxic_comments/train.py
+++ src/kaggle/toxic_comments/train.py
@@ -1,16 +1,29 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 import tensorflow.contrib.keras as keras
 
-HOME_DIR = 'kaggle/toxic_comments'
-dataset  = pd.read_csv("{}/train.csv".format(HOME_DIR), sep=',')
+# 1. Load data into memory.
+
+PWD     = 'kaggle/toxic_comments'
+dataset = pd.read_csv('{}/train.csv'.format(PWD), sep=',')
 
 X = dataset['comment_text']
 Y = dataset['toxic']
 
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
+# 2. Preprocess text fields.
+
 tokenizer = keras.preprocessing.text.Tokenizer(num_words=20000)
-tokenizer.fit_on_texts(X)
-sequences = tokenizer.texts_to_sequences(X)
-X = keras.preprocessing.sequence.pad_sequences(sequences, maxlen=300)
+tokenizer.fit_on_texts(X_train)
+
+sequences = tokenizer.texts_to_sequences(X_train)
+X_train   = keras.preprocessing.sequence.pad_sequences(sequences, maxlen=300)
+
+sequences = tokenizer.texts_to_sequences(X_test)
+X_test    = keras.preprocessing.sequence.pad_sequences(sequences, maxlen=300)
+
+# 3. Build model.
 
 model = keras.models.Sequential([
     keras.layers.Embedding(20000, 128, input_length=300),
@@ -25,8 +38,13 @@ model.compile(loss='binary_crossentropy',
 viz = keras.callbacks.TensorBoard(log_dir='logs',
                                   write_graph=True)
 
-model.fit(X, Y, epochs=12, batch_size=64, callbacks=[viz])
+# 4. Train model.
 
-model.evaluate(X, Y)
+model.fit(X_train, Y_train, epochs=1, batch_size=32, callbacks=[viz])
 
-print("\nComplete.")
+# 5. Test model.
+
+test_loss, test_acc = model.evaluate(X_test, Y_test)
+
+print("\nTest accuracy:", test_acc)
+print('Complete.')
