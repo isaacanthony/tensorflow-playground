7744d487b026e73ed29eb3d4ab54ffa2c901ad9e
Isaac Anthony <ianthony@optoro.com>
2018-01-09 14:06:44 -0500

    Predict six target variables.

--- src/kaggle/toxic_comments/train.py
+++ src/kaggle/toxic_comments/train.py
@@ -8,10 +8,29 @@ import tensorflow.contrib.keras as keras
 PWD     = 'kaggle/toxic_comments'
 dataset = pd.read_csv("{}/train.csv".format(PWD), sep=',')
 
-X = dataset['comment_text']
-Y = dataset['toxic']
+X  = dataset['comment_text']
+Y1 = dataset['toxic']
+Y2 = dataset['severe_toxic']
+Y3 = dataset['obscene']
+Y4 = dataset['threat']
+Y5 = dataset['insult']
+Y6 = dataset['identity_hate']
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+split    = train_test_split(X, Y1, Y2, Y3, Y4, Y5, Y6, test_size=0.2)
+X_train  = split[0]
+X_test   = split[1]
+Y1_train = split[2]
+Y1_test  = split[3]
+Y2_train = split[4]
+Y2_test  = split[5]
+Y3_train = split[6]
+Y3_test  = split[7]
+Y4_train = split[8]
+Y4_test  = split[9]
+Y5_train = split[10]
+Y5_test  = split[11]
+Y6_train = split[12]
+Y6_test  = split[13]
 
 # 2. Preprocess text fields.
 
@@ -29,29 +48,39 @@ X_test    = keras.preprocessing.sequence.pad_sequences(sequences, maxlen=300)
 
 # 3. Build model.
 
-model = keras.models.Sequential([
-    keras.layers.Embedding(20000, 128, input_length=300),
-    keras.layers.Dropout(0.2),
-    keras.layers.Conv1D(64, 5, activation='relu'),
-    keras.layers.MaxPooling1D(pool_size=4),
-    keras.layers.LSTM(128),
-    keras.layers.Dense(1, activation='sigmoid')
-])
+ins = keras.layers.Input(shape=(300,), dtype='int32')
+y   = keras.layers.Embedding(20000, 128)(ins)
+y   = keras.layers.Dropout(0.2)(y)
+y   = keras.layers.Conv1D(64, 5, activation='relu')(y)
+y   = keras.layers.MaxPooling1D(pool_size=4)(y)
+y   = keras.layers.LSTM(128)(y)
+y1  = keras.layers.Dense(1, activation='sigmoid')(y)
+y2  = keras.layers.Dense(1, activation='sigmoid')(y)
+y3  = keras.layers.Dense(1, activation='sigmoid')(y)
+y4  = keras.layers.Dense(1, activation='sigmoid')(y)
+y5  = keras.layers.Dense(1, activation='sigmoid')(y)
+y6  = keras.layers.Dense(1, activation='sigmoid')(y)
 
-model.compile(loss='binary_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
+model = keras.models.Model(inputs=ins, outputs=[y1, y2, y3, y4, y5, y6])
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 viz  = keras.callbacks.TensorBoard(log_dir='logs', write_graph=True)
 save = keras.callbacks.ModelCheckpoint("{}/model.hdf5".format(PWD), period=1)
 
 # 4. Train model.
 
-model.fit(X_train, Y_train, epochs=3, batch_size=32, callbacks=[viz, save])
+model.fit(X_train,
+          [Y1_train, Y2_train, Y3_train, Y4_train, Y5_train, Y6_train],
+          epochs=3,
+          batch_size=32,
+          callbacks=[viz, save])
 
 # 5. Test model.
 
-test_loss, test_acc = model.evaluate(X_test, Y_test)
+results = model.evaluate(X_test,
+                         [Y1_test, Y2_test, Y3_test, Y4_test, Y5_test, Y6_test])
 
-print("\nTest accuracy:", test_acc)
+print("\nLoss:", results[0])
+print('Accuracy:', results[-6:])
 print('Complete.')
