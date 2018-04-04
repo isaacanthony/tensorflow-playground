f816e89dd907690c4d8660c81c3ba182f4d76a38
Isaac Anthony <ianthony@optoro.com>
2018-04-04 11:27:35 -0400

    Save model in between epochs.

--- src/pinkeye/train.py
+++ src/pinkeye/train.py
@@ -1,6 +1,9 @@
+import os.path
 import tensorflow.contrib.keras as keras
 
 # 1. Import non-trainable inception model
+
+PWD = 'pinkeye'
 
 base = keras.applications.inception_v3.InceptionV3(
     include_top=False,
@@ -17,22 +20,31 @@ for layer in base.layers:
 
 y = base.output
 y = keras.layers.Dense(64, input_dim=1000, activation='relu')(y)
-y = keras.layers.Dense(1, activation='sigmoid')(y)
+y = keras.layers.Dense(32, activation='relu')(y)
+y = keras.layers.Dense(3, activation='sigmoid')(y)
 
-model = keras.models.Model(inputs=base.input, outputs=y)
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+path = "{}/model.hdf5".format(PWD)
+if os.path.isfile(path):
+    model = keras.models.load_model(path)
+else:
+    model = keras.models.Model(inputs=base.input, outputs=y)
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # 3. Stream photos.
 
 train_datagen = keras.preprocessing.image.ImageDataGenerator()
 
 train_generator = train_datagen.flow_from_directory(
-    'pinkeye/downloads',
+    "{}/downloads".format(PWD),
     target_size=(299, 299),
     batch_size=32,
-    class_mode='binary')
+    class_mode='categorical')
+
+viz  = keras.callbacks.TensorBoard(log_dir='logs', write_graph=True)
+save = keras.callbacks.ModelCheckpoint(path, period=1)
 
 model.fit_generator(
     train_generator,
     steps_per_epoch=100,
-    epochs=3)
+    epochs=3,
+    callbacks=[viz, save])
